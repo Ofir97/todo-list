0c173ce505cec5618f8861239600f09072b15e7a
Pengoled <pengoled@gmail.com>
2021-12-06 19:55:25 +0200

    added todolist project

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require("express")
+const app = express()
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 80)
