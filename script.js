71802d3aebc376155bbc06cb16e502796d83afd5
Pengoled <pengoled@gmail.com>
2021-12-06 19:38:39 +0200

    added local storage

--- script.js
+++ script.js
@@ -4,9 +4,22 @@ const todoInput = document.getElementById("todo-input");
 const todosFooterContainer = document.getElementById("todos-footer-container");
 const pendingTasksMsg = document.getElementById("pending-tasks-msg");
 const addTodoBtn = document.getElementById("add-todo-btn");
-todoInput.focus();
-
 let todosCounter = 0;
+let todos = [];
+
+todoInput.focus();
+getFromLocalStorage();
+
+function getFromLocalStorage() {
+    const reference = localStorage.getItem('todos');
+    console.log(todos);
+    // if reference exists
+    if (reference) {
+        // converts back to array and store it in todos array
+        todos = JSON.parse(reference);
+        todos.forEach(todo => addTodo(todo));
+    }
+}
 
 todoInput.addEventListener("input", function () {
     if (todoInput.value.length === 0) {
@@ -16,14 +29,30 @@ todoInput.addEventListener("input", function () {
     }
 });
 
-todoInput.addEventListener("keyup", function(event) {
+todoInput.addEventListener("keyup", function (event) {
     if (event.key === 'Enter') {
         document.getElementById("add-todo-btn").click();
     }
 })
-  
 
-function addTodo() {
+
+function addTodo(todo) {
+    console.log(todo);
+
+    if (todo !== undefined) {
+        todoInput.value = todo.value;
+        addToLocalStorage(todos);
+    }
+
+    else {
+        todo = {
+            id: Date.now(),
+            value: todoInput.value
+        };
+        todos.push(todo);
+        addToLocalStorage(todos);
+    }
+
     todosCounter++;
     displayFooterContainer();
 
@@ -35,17 +64,41 @@ function addTodo() {
     removeLiBtn.id = `rmv-btn-${todosCounter}`;
     removeLiBtn.innerHTML = '-';
 
-    removeLiBtn.onclick = () => {
+    removeLiBtn.onclick = () => { //remove item button is clicked
         todosList.removeChild(liElement);
         todosCounter--;
         displayFooterContainer();
+        removeFromLocalStorage(todo.id)
+        console.log("!!!", todos);
     }
 
     todosList.appendChild(liElement);
     liElement.appendChild(removeLiBtn);
-    clearInput();
+
     todoInput.focus();
     addTodoBtn.disabled = true;
+
+    clearInput();
+    // console.log("!!!", todos);
+}
+
+function removeFromLocalStorage(id) {
+    console.log('id', id);
+    // filters out the <li> with the id and updates the todos array
+    todos = todos.filter(function (item) {
+        // use != not !==, because here types are different. One is number and other is string
+        return item.id != id;
+    })
+
+    addToLocalStorage(todos);
+    console.log('LOCAL STORAGE: ', localStorage.getItem('todos'));
+}
+
+
+function addToLocalStorage(todos) {
+    // conver the array to string then store it.
+    localStorage.setItem('todos', JSON.stringify(todos));
+    // console.log(localStorage.getItem('todos'));
 }
 
 function clearInput() {
@@ -71,4 +124,7 @@ function clearAllTodos() {
     }
 
     todoInput.focus();
+
+    todos = [];
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
